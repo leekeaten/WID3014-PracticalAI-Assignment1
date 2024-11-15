6b8275b478c2f764f8621b33e0bef25787e52dc1
LEEKEATEN <u2102836@siswa.um.edu.my>
2024-11-15 14:24:37 +0800

    load env variables from .env file

--- analyze.py
+++ analyze.py
@@ -2,9 +2,17 @@ from azure.cognitiveservices.vision.computervision import ComputerVisionClient
 from msrest.authentication import CognitiveServicesCredentials
 from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
 import time
+import os
+from dotenv import load_dotenv
 
-endpoint = "ENTER ENDPOINT HERE"
-key = "ENTER KEY HERE"
+# endpoint = "ENTER ENDPOINT HERE"
+# key = "ENTER KEY HERE"
+
+# Load environment variables from the .env file
+load_dotenv()
+
+endpoint = os.getenv("AZURE_ENDPOINT")
+key = os.getenv("AZURE_KEY")
 
 credentials = CognitiveServicesCredentials(key)
 
